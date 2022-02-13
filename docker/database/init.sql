315d921ee51493e38b01b854f23844644c0ee4a6
Matthias Burger <m.burger@team.jobs>
2022-02-13 19:20:02 +0100

    fixed docker

--- docker/database/init.sql
+++ docker/database/init.sql
@@ -1,2 +1,5 @@
 drop database if exists time_series;
 create database time_series;
+
+CREATE USER 'matthias'@'localhost' IDENTIFIED BY 'new_password' PASSWORD EXPIRE NEVER;
+GRANT ALL ON time_series.* TO 'matthias'@'localhost';
